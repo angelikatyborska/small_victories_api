758746f682640a4307795b7cb5691da9b48ccb99
Angelika Tyborska <atyborska93@gmail.com>
2016-02-05 16:03:26 +0100

    Add votes to user details

--- app/serializers/api/v1/user_details_serializer.rb
+++ app/serializers/api/v1/user_details_serializer.rb
@@ -3,7 +3,12 @@ class Api::V1::UserDetailsSerializer < Api::V1::UserSerializer
     attributes :id, :body, :created_at
   end
 
+  class VoteSerializer < Api::V1::BaseSerializer
+    attributes :value, :id, :victory_id
+  end
+
   attributes :email
 
   has_many :victories, serializer: VictorySerializer
+  has_many :votes, serializer: VoteSerializer
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe Api::V1::UsersController do
     context 'user exists' do
       let!(:user) { create :user }
       let!(:victory) { create :victory, user: user }
+      let!(:vote) { create :vote, user: user }
 
       subject { xhr :get, :show, nickname: user.nickname }
 
@@ -37,7 +38,7 @@ RSpec.describe Api::V1::UsersController do
       end
 
       it 'includes only the right keys' do
-        expect(JSON.parse(subject.body).keys).to match_array ['id', 'nickname', 'email', 'victories']
+        expect(JSON.parse(subject.body).keys).to match_array ['id', 'nickname', 'email', 'victories', 'votes']
       end
 
       it 'returns the user with correct data' do
@@ -46,9 +47,8 @@ RSpec.describe Api::V1::UsersController do
         expect(parsed_response['id']).to eq user.id
         expect(parsed_response['nickname']).to eq user.nickname
         expect(parsed_response['email']).to eq user.email
-        expect(parsed_response['victories']).to eq(
-          [{ 'id' => victory.id, 'body' => victory.body, 'created_at' => victory.created_at.utc.iso8601 }]
-        )
+        expect(parsed_response['victories'][0].keys).to match_array ['body', 'id', 'created_at']
+        expect(parsed_response['votes'][0].keys).to match_array ['id', 'value', 'victory_id']
       end
     end
   end
