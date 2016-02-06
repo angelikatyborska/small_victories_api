5caf263e6ea1e02c385caee97943fb87004d33ba
Angelika Tyborska <atyborska93@gmail.com>
2016-02-06 13:35:34 +0100

    Allow user url with username with a dot

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
 
-      resources :users, only: [:index, :show], param: :nickname
+      resources :users, only: [:index, :show], param: :nickname, constraints: { :nickname => /[^\/]+/ }
       resources :victories, only: [:index, :show, :create, :destroy] do
         resources :votes, only: [:index, :create, :destroy, :update]
       end
