819911425e4eaab793b3fe05330f677a4872505e
Angelika Tyborska <atyborska93@gmail.com>
2016-02-06 13:43:49 +0100

    Update seeds
    More votes looks better.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ User.destroy_all
 
 puts 'Creating users...'
 
-users_data = 16.times.with_object([]) do |n, users_data|
+users_data = 50.times.with_object([]) do |n, users_data|
   users_data << {
     email: "user#{ n }@example.com",
-    nickname: Faker::Internet.user_name,
+    nickname: "#{Faker::Internet.user_name}_n",
     password: 'password',
     confirmed_at: Time.zone.now
   }
@@ -21,7 +21,7 @@ users = User.create!(users_data)
 puts 'Creating victories...'
 
 victories_data = users.each.with_object([]) do |user, victories_data|
-  4.times do
+  2.times do
     victories_data << {
       user_id: user.id,
       body: Faker::Lorem.paragraph(3)
@@ -34,7 +34,7 @@ victories = Victory.create!(victories_data)
 puts 'Creating votes...'
 
 votes_data = victories.each_with_object([]) do |victory, votes_data|
-  users.sample(5).each do |user|
+  users.sample(10).each do |user|
     votes_data << { user: user, victory: victory, value: [1, -1].sample }
   end
 end
